11962fe6d76a7cde889c99b698e29b836e838c61
JonasChen <tom200425@gmail.com>
2025-02-25 09:40:00 +0800

    本地端Run專案 自動產https憑證

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import basicSsl from '@vitejs/plugin-basic-ssl' //本地端Run自動產https憑證
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), basicSsl()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src') //設定import @是從src開始
